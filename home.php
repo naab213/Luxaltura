d93eabfe71bb97eb29ca1fa7ef2ddb1dc56a3f42
Dorianrst <102527310+Dorianrst@users.noreply.github.com>
2025-03-23 22:43:54 +0100

    [PHASE 2] FIN

--- home.php
+++ home.php
@@ -32,7 +32,7 @@ session_start(); // Démarrez la session pour gérer l'état de connexion
 
     <nav>
         <ul>
-            <li><a href="presentation.html" title="Our presentation">Presentation</a></li>
+            <li><a href="presentation.php" title="Our presentation">Presentation</a></li>
             <li><a href="specific.php" title="Go to my bookings">Bookings</a></li>
             <li><a href="#contact" title="Go to contact">Contact us</a></li>
         </ul>
