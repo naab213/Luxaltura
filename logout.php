c84e5f064f50cd9d618204bf7c164c46fcef151e
Dorianrst <102527310+Dorianrst@users.noreply.github.com>
2025-03-23 15:55:39 +0100

    [PHASE 2]Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
+
+if (isset($_SESSION['user_id'])) {
+    // Supprimer le fichier temporaire
+    $tempFilePath = 'dataJSON/temp/user_' . $_SESSION['user_id'] . '.json';
+    if (file_exists($tempFilePath)) {
+        unlink($tempFilePath);
+    }
+}
+
+// Détruire la session
 session_unset();
 session_destroy();
+
+// Rediriger vers la page de connexion
 header("Location: sign_in.php");
 exit();
 ?>
